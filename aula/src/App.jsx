403b404773542b52f3d7c8767786933e8c35a253
Luansramos <luanramosgarciadesouza014@gmail.com>
2024-08-16 22:33:10 -0300

    primeiro teste de reacao

--- aula/src/App.jsx
+++ aula/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import reactLogo from './assets/react.svg'
+import Header from './componentes/Header'
+import viteLogo from '/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Header />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ aula/src/componentes/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+
+    return (
+        <>
+            <h1></h1>
+            <p>vai se fuder matheus</p>
+        </>
+    )
+}
+export default Header
